f3e75923da32a1d45b40638b2451405013842501
Nicholas Lindvay <Nicholas.Lindvay@logisticsplus.com>
2024-10-31 10:25:20 -0400

    add sample class

---
+++ src/mod.one.lib/ModOneClass.cs
@@ -0,0 +1,10 @@
+namespace mod.one.lib
+{
+    public class ModOneClass
+    {
+        public string GetModOneString()
+        {
+            return "ModOneClass.GetModOneString()";
+        }
+    }
+}
